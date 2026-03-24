4c6f1283f504ccf7d0617bd42d87ed36ca89e276
Madhumati08 <madhumatisavoor08@gmai.com>
2026-03-24 16:39:50 +0530

    added terraform files

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux (update if needed)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
