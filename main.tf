04ab63b9f836fa4aaec52871fa2691f9a637e7ca
Madhumati08 <madhumatisavoor08@gmai.com>
2026-03-24 16:57:36 +0530

    chnaged intance id

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0f5ee92e2d63afc18" 
-  instance_type = "t2.micro"
+  ami           = "ami-0f559c3642608c138" 
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform-EC2"
